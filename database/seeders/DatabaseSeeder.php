99afbea7af526c3ffb6a7bb57b4eb37fede40eb5
sebugueroaljay2 git config --global user.email sebugueroaljay2@example.com <sebugueroaljay10@gmail.com>
2025-08-12 18:37:14 +0800

    fifth commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(RoleSeeder::class);
+        // $this->call(RoleSeeder::class);
         // $this->call(AdminSeeder::class);
     }
 }
